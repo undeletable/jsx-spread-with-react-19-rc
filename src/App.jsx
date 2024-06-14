054a7454eaf9c1b4c005604b04e839e9e70ca507
Max Shepel <max@undeletable.name>
2024-06-15 00:14:13 +0300

    Implement example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isCountButtonActive, setIsButtonActive] = useState(true);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        gap: '5px',
+        justifyContent: 'flex-start'
+      }}
+    >
+      <button
+        disabled={!isCountButtonActive}
+        {...(isCountButtonActive && {
+          onClick: () => setCount(currentCount => currentCount + 1)
+        })}
+      >
+        Increment count
+      </button>
+      <button onClick={() => setIsButtonActive(currentState => !currentState)}>
+        Toggle count button state
+      </button>
+      <span>Current count: {count}. Increment {isCountButtonActive ? 'enabled' : 'disabled'}</span>
+    </div>
+  );
 }
 
-export default App
+export default App;
